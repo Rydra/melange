2f2ac3bb0d8ddc5369e4755df616217548144d5c
David Jiménez <david@21buttons.com>
2017-10-18 17:01:37 +0200

    Temporarily removing the pybuilder plugin

--- build.py
+++ build.py
@@ -5,7 +5,8 @@ use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-use_plugin('pypi:pybuilder_pytest')
+
+#use_plugin('pypi:pybuilder_pytest')
 
 name = "melange"
 version = "1.0-arrakis"
