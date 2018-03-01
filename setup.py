b651533e072343476a715176d67faecadc7f5056
David Jiménez <david@21buttons.com>
2018-03-01 12:40:57 +0100

    Make the AWS Driver a bit more customizable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='melange',
-      version='3.1.1',
+      version='3.1.2',
       description='A messaging library for an easy inter-communication in distributed and microservices architectures',
       url='https://github.com/Rydra/melange',
       author='David Jiménez (Rydra)',
