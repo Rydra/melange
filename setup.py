75b4e2dff940e72125bc4d9f435fb1a2a3c657cb
David Jiménez <david@21buttons.com>
2018-05-08 17:52:17 +0200

    Set Melange version to Melange 4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='melange',
-      version='3.1.3',
+      version='4.0.0',
       description='A messaging library for an easy inter-communication in distributed and microservices architectures',
       url='https://github.com/Rydra/melange',
       author='David Jiménez (Rydra)',
